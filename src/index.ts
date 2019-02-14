a963f7809dd8cb3b36d04277e94fdf4d7a60008f
Amit Sengar <amitsengar@yahoo.com>
2019-02-14 17:46:16 +0530

    Setup Package

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export const Greeter = (name: string) => `Hello ${name}`;
+// export * from './payment.routes'
+
+// exports.printMsg = function() {
+//     console.log("This is a message from the demo package");
+//   }
